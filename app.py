5bdd35d1f2772f0a9dac2c374c94a9563fcfafd0
luke0623 <luke0623@outlook.com>
2022-09-01 18:14:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 from uvicorn import run
 
 if __name__ == "__main__":
-	run("connect_server.main:app", host="127.0.0.1", port=8888, log_level="info", workers=1)
+	run("connect_server.main:app", host="127.0.0.1", port=5000, log_level="info", workers=1)
